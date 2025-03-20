d3b2d10838b4981cd430947539350ece91950ceb
Ayşegül Avcu <aysegul.avcu@hotmail.com>
2025-03-20 15:00:20 +0300

    tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Header } from "@/components/layout/header"
 import { Footer } from "@/components/layout/footer"
 import { Inter as FontSans } from "next/font/google"
 import "./globals.css"
+import Script from "next/script"
 
 export const metadata: Metadata = {
   title: "Tekelmatik",
@@ -25,6 +26,16 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <Script
+          id="google-tag-manager"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-KDGQTK7L');`
+          }}
+        />
         <link
           rel="preload"
           href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
@@ -41,6 +52,14 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes" />
       </head>
       <body className="flex min-h-screen flex-col">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KDGQTK7L"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <ThemeProviderWrapper>
           <QueryProvider>
             <Header />
